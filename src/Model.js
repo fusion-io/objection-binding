803b1cc856909023b0463572365d0dc83400bb43
rikky <son.levuthai@gmail.com>
2019-07-16 03:42:56 +0700

    make Model can be type casted

--- src/Model.js
+++ src/Model.js
@@ -7,4 +7,23 @@ export default class Model extends BaseModel {
     static get tableName() {
         return snakeCase(pluralize(this.name));
     }
+
+    static get $typeCastings() {
+        return [];
+    }
+
+    $parseDatabaseJson(json) {
+        json = super.$parseDatabaseJson(json);
+
+        let willBeParsedJson = Object.assign({}, json);
+
+        willBeParsedJson.$original = json;
+
+        this.constructor
+            .$typeCastings
+            .forEach(({key, caster}) => willBeParsedJson[key] = caster.parseDatabase(json, key))
+        ;
+
+        return willBeParsedJson;
+    }
 }
