0997a3cc771f7f6c7910385c07ea9626a835594d
rikky <son.levuthai@gmail.com>
2019-07-16 10:01:32 +0700

    handle null cases

--- src/types/moment.js
+++ src/types/moment.js
@@ -2,15 +2,38 @@ import moment from "moment";
 
 export default {
     parseDatabase(json, original, key) {
+
+        if (original[key] === null) {
+            return json[key] = null;
+        }
+
         return json[key] = moment(original[key]);
     },
+
     formatDatabase(json, original, key) {
+
+        if (original[key] === null) {
+            return json[key] = null;
+        }
+
         return json[key] = original[key].toDate().getTime();
     },
+
     parseJson(json, original, key) {
+
+        if (original[key] === null) {
+            return json[key] = null;
+        }
+
         return json[key] = moment(original[key]);
     },
+
     formatJson(json, original, key) {
+
+        if (original[key] === null) {
+            return json[key] = null;
+        }
+
         return json[key] = original[key].toDate().toISOString();
     },
 }
