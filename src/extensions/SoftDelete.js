89e2be8f65f8f7dc12c7a441b26ebaa96ab70966
rikky <son.levuthai@gmail.com>
2019-07-16 20:28:04 +0700

    docs

--- src/extensions/SoftDelete.js
+++ src/extensions/SoftDelete.js
@@ -1,27 +1,75 @@
 export default ({column}) => Base => {
-    return class SoftDelete extends Base {
-        delete() {
-            this.mergeContext({softDelete: column});
 
+    return class SoftDelete extends Base {
+
+        constructor(...args) {
+            super(...args);
+
+            this.applyTrashFilter();
+        }
+
+        /**
+         * Apply the trash filter.
+         */
+        applyTrashFilter() {
+            this.mergeContext({softDelete: true, trashFilter: true});
+
+            this.onBuild(builder => {
+                const {trashFilter} = this.context();
+
+                if (builder.isFind()) {
+                    return trashFilter ? builder.whereNull(column) : builder;
+                }
+
+                return builder;
+            });
+        }
+
+        /**
+         * Perform soft delete
+         *
+         * @return {Objection.QueryBuilderYieldingCount<QM, RM> | *}
+         */
+        delete() {
             return this.patch({[column]: new Date()});
         }
 
+        /**
+         * Restore the deleted model
+         *
+         * @return {Objection.QueryBuilderYieldingCount<QM, RM> | *}
+         */
         restore() {
-            this.mergeContext({softDeleteRestore: column});
-
             return this.patch({[column]: null});
         }
 
+        /**
+         * Destroy the models (hard delete)
+         *
+         * @return {Objection.QueryBuilderYieldingCount<QM, RM>|*|Knex.QueryBuilder<TRecord, DeferredIndex.Augment<UnwrapArrayMember<TResult>, TRecord, StrKey<TRecord>>[]>|Knex.QueryBuilder<TRecord, DeferredKeySelection.Augment<UnwrapArrayMember<TResult>, TRecord, StrKey<TRecord>>[]>|Knex.QueryBuilder<TRecord, any>|Knex.QueryBuilder<TRecord, number>|Objection.QueryBuilderYieldingCount<QM, RM>|boolean|Promise<boolean>|void|IDBRequest}
+         */
         destroy() {
             return super.delete();
         }
 
+        /**
+         * Search for models that is soft deleted
+         *
+         * @return {Knex.QueryBuilder<TRecord, TResult> | Objection.QueryBuilder<QM, RM, RV>}
+         */
         inTrash() {
-            return this.whereNotNull(`${this.modelClass().tableName}.${column}`);
+            this.mergeContext({trashFilter: false});
+
+            return this.whereNotNull(column);
         }
 
+        /**
+         * Search for models even it is soft deleted
+         *
+         * @return {SoftDelete}
+         */
         includeTrash() {
-
+            return this.mergeContext({trashFilter: false});
         }
     }
 }
